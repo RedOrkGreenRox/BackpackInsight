d516dc14ebbc8583e70d51a799a89328f7608422
User <redcreeper001@gmail.com>
2026-02-04 15:24:15 +0300

    Main buttons refactor (sidebar). Automated push: 04.02.2026 15:24:15,65

--- auto_push.bat
+++ auto_push.bat
@@ -23,7 +23,7 @@ git add --renormalize .
 git add -A
 
 echo Создаю коммит...
-git commit -m "Main buttons Optional (avif/webp). Automated push: %date% %time%"
+git commit -m "Main buttons refactor (sidebar). Automated push: %date% %time%"
 
 echo Отправляю в удаленный репозиторий...
 git push origin HEAD
