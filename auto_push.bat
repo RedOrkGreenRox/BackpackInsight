7c20da20e7831360e046af5940af3e476544f1ff
User <redcreeper001@gmail.com>
2026-02-04 11:13:29 +0300

    Automated push: 04.02.2026 11:13:29,42

--- auto_push.bat
+++ auto_push.bat
@@ -1,8 +1,24 @@
 @echo off
-echo Adding all changes...
-git add .
-echo Committing changes...
-git commit -m "Automated push"
-echo Pushing to remote...
-git push
-echo Done!
+chcp 65001 > nul
+
+:: Переходим в папку, где лежит сам этот .bat файл
+cd /d "%~dp0"
+
+echo Текущая папка: %cd%
+
+if not exist ".gitattributes" (
+    echo [ОШИБКА] .gitattributes не найден!
+    echo Проверь, лежит ли этот .bat в той же папке, что и файл.
+    dir /b .git*
+    pause
+    exit /b
+)
+
+echo [OK] .gitattributes найден. Начинаю пуш...
+
+git add -A
+git commit -m "Automated push: %date% %time%"
+git push origin HEAD
+
+echo Готово!
+pause
