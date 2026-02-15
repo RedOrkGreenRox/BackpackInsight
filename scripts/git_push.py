440bb062cdd3b9f4bf9099303f335d09870abe24
User <redcreeper001@gmail.com>
2026-02-15 16:20:16 +0300

    Image  recovery | Automated push: 2026-02-15 16:20:16

--- scripts/git_push.py
+++ scripts/git_push.py
@@ -47,7 +47,7 @@ def main():
 
     # 6. Создание сообщения
     timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    upd = "Image recovery"
+    upd = "Image  recovery"
     message = f"{upd} | Automated push: {timestamp}"
 
     # 7. Коммит
