805c1293e2a782deb0133531bd6cb254fe91e7a7
User <redcreeper001@gmail.com>
2026-03-31 22:39:16 +0300

    4.0.0 | Automated push: 2026-03-31 22:39:16

--- scripts/git_push.py
+++ scripts/git_push.py
@@ -47,7 +47,7 @@ def main():
 
     # 6. Создание сообщения
     timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    upd = "screenshot fix"
+    upd = "4.0.0"
     message = f"{upd} | Automated push: {timestamp}"
 
     # 7. Коммит
